1b43abfecbb2ecf34bc6566377c8a6079976102a
Vikram Ramavarapu <vikramr2@illinois.edu>
2023-05-02 15:21:38 -0500

    running modified summary

--- run.sh
+++ run.sh
@@ -4,3 +4,7 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 python3 $DIR/scripts/change_format.py $1 &> /dev/null
 python3 $DIR/scripts/json_compression.py $1
 
+filename=$(basename "$1")
+arg="$1/$filename.json"
+
+python3 $DIR/scripts/summarize.py $arg
